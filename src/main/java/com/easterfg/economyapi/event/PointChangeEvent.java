35fed5a7c1ad8e7f6fa7f10b0bcbfd8245a5ccef
EasterFG <EasterFG@gmail.com>
2019-08-09 17:23:24 +0800

    修复了监听报错的问题

--- src/main/java/com/easterfg/economyapi/event/PointChangeEvent.java
+++ src/main/java/com/easterfg/economyapi/event/PointChangeEvent.java
@@ -2,8 +2,15 @@ package com.easterfg.economyapi.event;
 
 import cn.nukkit.event.Cancellable;
 import cn.nukkit.event.Event;
+import cn.nukkit.event.HandlerList;
 
 public class PointChangeEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    public static HandlerList getHandlers() {
+        return handlers;
+    }
 
     public static final int ACTION_ADD = 0;
     public static final int ACTION_REDUCE = 1;
